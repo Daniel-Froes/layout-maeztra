b190530c2c02bf1254c0e67c83ab05fa8f0827b7
Daniel Fróes <daniel.froes@driven.cx>
2023-12-29 11:35:06 -0300

    chore: first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        titillium: ['"Titillium Web"'],
+        lato: ["Lato"],
+      },
+    },
+  },
+  plugins: [],
+};
